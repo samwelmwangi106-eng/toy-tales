745de290cf31c24112832392a17aec0e7133032b
Samwel <samwelmwangi106@gmail.com>
2026-05-14 15:46:50 +0300

    Added like toy

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,30 +1,81 @@
 import React from "react";
 
-function ToyCard({toy, onDeleteToy}) {
+function ToyCard({ toy, toys, setToys }) {
 
-  function handleDelete(){
-    fetch(`http://localhost:3000/toys/${toy.id}`,{
+  // DELETE TOY
+  function handleDelete() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
       method: "DELETE",
     }).then(() => {
+
       const updatedToys = toys.filter(
         (singleToy) => singleToy.id !== toy.id
       );
 
       setToys(updatedToys);
+
     });
+  }
+
+  // LIKE TOY
+  function handleLike() {
+
+    const updatedLikes = toy.likes + 1;
+
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        likes: updatedLikes,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+
+        const updatedToys = toys.map((singleToy) => {
+
+          if (singleToy.id === updatedToy.id) {
+            return updatedToy;
+          } else {
+            return singleToy;
+          }
+
+        });
+
+        setToys(updatedToys);
+
+      });
   }
 
   return (
     <div className="card" data-testid="toy-card">
+
       <h2>{toy.name}</h2>
+
       <img
         src={toy.image}
         alt={toy.name}
         className="toy-avatar"
       />
+
       <p> Likes:{toy.likes} </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+
+      <button
+        className="like-btn"
+        onClick={handleLike}
+      >
+        Like {"<3"}
+      </button>
+
+      <button
+        className="del-btn"
+        onClick={handleDelete}
+      >
+        Donate to GoodWill
+      </button>
+
     </div>
   );
 }
